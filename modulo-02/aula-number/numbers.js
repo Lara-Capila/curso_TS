61decf3678dc23fb1bbc561d37cc61a24ddd9c3a
Lara-Capila <laracapilak@gmail.com>
2021-11-25 11:56:08 -0300

    Aula sobre o tipo primitivo number

---
+++ modulo-02/aula-number/numbers.js
@@ -0,0 +1,13 @@
+"use strict";
+const number1 = 23.6;
+const number2 = 0x78CF;
+const number3 = 0o577;
+const number4 = 0b110001;
+console.log(number1);
+console.log(number2);
+console.log(number3);
+console.log(number4);
+console.log(typeof number1);
+console.log(typeof number2);
+console.log(typeof number3);
+console.log(typeof number4);
---
+++ modulo-02/aula-number/numbers.ts
@@ -0,0 +1,14 @@
+const number1: number = 23.6;
+const number2: number = 0x78CF;
+const number3: number = 0o577;
+const number4: number = 0b110001;
+
+console.log(number1);
+console.log(number2);
+console.log(number3);
+console.log(number4);
+
+console.log(typeof number1);
+console.log(typeof number2);
+console.log(typeof number3);
+console.log(typeof number4);
