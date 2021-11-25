7b563b150c880ca0d2960f49f381c305631f90fd
Lara-Capila <laracapilak@gmail.com>
2021-11-25 12:57:48 -0300

    Aula sobre o tipo string

---
+++ modulo-02/aula-string/stringTypes.js
@@ -0,0 +1,7 @@
+"use strict";
+const primeiroNome = "Lara";
+const ultimoNome = "Capila";
+function nomeCompleto(primeiroNome, segundoNome) {
+    return `Olá ${primeiroNome} ${segundoNome}`;
+}
+console.log(nomeCompleto(primeiroNome, ultimoNome));
---
+++ modulo-02/aula-string/stringTypes.ts
@@ -0,0 +1,9 @@
+
+const primeiroNome: string = "Lara";
+const ultimoNome: string = "Capila";
+
+function nomeCompleto(primeiroNome: string, segundoNome: string) :string {
+  return `Olá ${primeiroNome} ${segundoNome}`;
+}
+
+console.log(nomeCompleto(primeiroNome, ultimoNome));
