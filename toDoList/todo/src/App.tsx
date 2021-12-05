f830aaaeb34191d531b246b33d7abe6c14290366
Lara Capila <laracapilak@gmail.com>
2021-12-04 21:10:34 -0300

    feat: cria arquivo principal da aplicação e seu estilo

--- toDoList/todo/src/App.tsx
+++ toDoList/todo/src/App.tsx
@@ -1,9 +1,26 @@
-// import React from 'react';
-
+import React from 'react';
+import * as Styles from './App.styles';
+import ListItem from './components/ListIstem';
+import { Item } from './types/Item';
 
 function App() {
+  const [list, setList] = React.useState<Item[]>([{
+    id: 1,
+    name: 'comprar',
+    done: false,
+  }]);
+
   return (
-    <h1>hello</h1>
+    <Styles.Container>
+      <Styles.Area>
+        <Styles.Header>
+          Lista de Tarefas
+        </Styles.Header>
+        {list.map((tarefa, index) => (
+            <ListItem tarefa={ tarefa } key={ index } />
+          ))}
+      </Styles.Area>
+    </Styles.Container>
   );
 }
 
