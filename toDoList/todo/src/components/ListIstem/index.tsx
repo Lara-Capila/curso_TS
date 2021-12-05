d55eaac28ff16d0e0560e64a83c4b19815f2a3ee
Lara-Capila <laracapilak@gmail.com>
2021-12-04 22:47:56 -0300

    feat(ListItem): Implementa lógica do checkbox, alterando estado e riscando a task

--- toDoList/todo/src/components/ListIstem/index.tsx
+++ toDoList/todo/src/components/ListIstem/index.tsx
@@ -2,17 +2,22 @@ import React from "react";
 
 import * as Styled from './styles';
 import {Item} from '../../types/Item'
-import Checkbox from "../InputCheckbox";
+// import Checkbox from "../InputCheckbox";
 
 type Props = {
   tarefa: Item,
+  onChange: (id: number, done: boolean) => void,
 }
 
-function ListItem({tarefa}: Props) {
+function ListItem({tarefa, onChange}: Props) {
+
   return (
-    <Styled.Container>
-      <Checkbox />
-      {tarefa.name}
+    <Styled.Container status={tarefa.done}>
+      <input
+        type="checkbox"
+        checked={tarefa.done}
+        onChange={e => onChange(tarefa.id, e.target.checked) } />
+      <label>{tarefa.name}</label>
     </Styled.Container>
   )
 }
--- toDoList/todo/src/components/ListIstem/styles.ts
+++ toDoList/todo/src/components/ListIstem/styles.ts
@@ -1,3 +1,27 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+type Props = {
+  status: boolean
+}
+
+export const Container = styled.div(({status}: Props) => (
+  `
+    display: flex;
+    background-color: #20212c;
+    padding: 10px;
+    border-radius: 10px;
+    margin: 20px 0 10px;
+    align-items: center;
+
+    label {
+      color: #ccc;
+      text-decoration: ${status ? 'line-through' : 'initial'}
+    }
+    
+    input {
+      width: 18px;
+      height: 18px;
+      margin-right: 8px;
+    }
+  `
+))
