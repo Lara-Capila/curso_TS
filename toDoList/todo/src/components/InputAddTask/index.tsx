e14158d19c098d385234f7c1d7ce6c89c9215fd1
Lara-Capila <laracapilak@gmail.com>
2021-12-04 23:14:40 -0300

    feat(InputAddTask): implementa evento de teclado e estado para armazenar valor do input

--- toDoList/todo/src/components/InputAddTask/index.tsx
+++ toDoList/todo/src/components/InputAddTask/index.tsx
@@ -1,13 +1,31 @@
-import React from "react";
+/* eslint-disable @typescript-eslint/no-unused-expressions */
+import React, { useState, KeyboardEvent } from "react";
 import * as Styled from './styles';
 
-function InputAddTask() {
+type Props = {
+  onEnter: (taskName: string) => void
+}
+
+function InputAddTask({ onEnter } : Props) {
+  const [inputText, setInputText] = useState('');
+
+  const handleKeyUp = (e: KeyboardEvent) => {
+    if (e.code === 'Enter' && inputText !== '') {
+      onEnter(inputText);
+      setInputText('');
+    }
+  }
+
   return (
     <Styled.Container>
       <div className="emoji-plus">➕</div>
       <input
         type="text"
         placeholder="Adicone uma tarefa... "
+        value={ inputText }
+        // refatorar esse onChange
+        onChange={ e => setInputText(e.target.value) }
+        onKeyUp={ handleKeyUp }
       />
     </Styled.Container>
   );
