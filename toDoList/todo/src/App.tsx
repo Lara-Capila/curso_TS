081dbe15caddd549246e1e28b4b763bc00f3fa08
Lara-Capila <laracapilak@gmail.com>
2021-12-04 22:48:39 -0300

    feat(ListItem): Implementa lógica para alterar chave done da task

--- toDoList/todo/src/App.tsx
+++ toDoList/todo/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import * as Styles from './App.styles';
+import InputAddTask from './components/InputAddTask';
 import ListItem from './components/ListIstem';
 import { Item } from './types/Item';
 
@@ -10,14 +11,30 @@ function App() {
     done: false,
   }]);
 
+  const handleTaskStatus = (id: number, done: boolean): void => {
+    const updateStatusTask = [...list];
+
+    for (let i in updateStatusTask) {
+      if(updateStatusTask[i].id === id) {
+        updateStatusTask[i].done = done;
+      }
+    }
+    setList(updateStatusTask);
+  }
+
   return (
     <Styles.Container>
       <Styles.Area>
         <Styles.Header>
           Lista de Tarefas
+          <InputAddTask />
         </Styles.Header>
         {list.map((tarefa, index) => (
-            <ListItem tarefa={ tarefa } key={ index } />
+            <ListItem
+              tarefa={ tarefa }
+              key={ index }
+              onChange={handleTaskStatus}
+            />
           ))}
       </Styles.Area>
     </Styles.Container>
